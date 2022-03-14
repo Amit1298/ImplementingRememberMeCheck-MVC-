e4a732c034160803857643c2149696c126372a5b
Amit <amitkr24031995@gmail.com>
2022-03-14 16:38:49 +0530

    Adding ImplementingRememberMeCheck

---
+++ ImplementingRememberMeCheck/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace ImplementingRememberMeCheck.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            if (Session["Username"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        public ActionResult Logout()
+        {
+            if(Session["Username"] != null)
+            {
+                Session.Abandon();
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
